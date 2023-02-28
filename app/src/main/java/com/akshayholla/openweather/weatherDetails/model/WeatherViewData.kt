602e0df53bec94b7e6c2cfd7e75ba3617373d9e2
akuholla <akuholla@gmail.com>
2023-02-28 11:22:42 -0800

    Creates weather view data model

---
+++ app/src/main/java/com/akshayholla/openweather/weatherDetails/model/WeatherViewData.kt
@@ -0,0 +1,19 @@
+package com.akshayholla.openweather.weatherDetails.model
+
+class WeatherViewData(
+    val cityName: String,
+    val temperature: String,
+    val feelsLike: String,
+    val minTemp: String,
+    val maxTemp: String,
+    val title: String,
+    val description: String,
+    val icon: String,
+    val pressure: String,
+    val humidity: String,
+    val windSpeed: String,
+    val windDirection: String,
+    val sunrise: String,
+    val sunset: String,
+) {
+}
