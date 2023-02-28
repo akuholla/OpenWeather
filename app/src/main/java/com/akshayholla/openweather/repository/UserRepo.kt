02aa21687fd76ca4013e6d3449087c859f6a8a9a
akuholla <akuholla@gmail.com>
2023-02-28 11:25:42 -0800

    Checks in UserRepo

---
+++ app/src/main/java/com/akshayholla/openweather/repository/UserRepo.kt
@@ -0,0 +1,14 @@
+package com.akshayholla.openweather.repository
+
+import com.akshayholla.openweather.data.SharedPrefDataSource
+import com.akshayholla.openweather.location.model.LocationData
+import javax.inject.Inject
+
+class UserRepo @Inject constructor(
+    val sharedPrefDataSource: SharedPrefDataSource
+) {
+    fun getLocationData() = sharedPrefDataSource.getSavedLocationData
+
+    suspend fun saveLocationData(locationData: LocationData) =
+        sharedPrefDataSource.saveLocationData(locationData = locationData)
+}
