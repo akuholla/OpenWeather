75081339b2794e77a3f2398599a5af500d5b083b
akuholla <akuholla@gmail.com>
2023-02-28 11:26:27 -0800

    Wires up activity with viewmodel and view composables

--- app/src/main/java/com/akshayholla/openweather/MainActivity.kt
+++ app/src/main/java/com/akshayholla/openweather/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.akshayholla.openweather
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
 import com.akshayholla.openweather.ui.theme.OpenWeatherTheme
+import com.akshayholla.openweather.weatherDetails.WeatherAppScreen
+import com.akshayholla.openweather.weatherDetails.WeatherDetailsViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    val weatherDetailsViewModel : WeatherDetailsViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //TODO: Explain user about why we would like their location access before asking them for permission
+        if (isLocationPermissionNotGranted()) {
+            locationPermissionRequest.launch(
+                arrayOf(
+                    android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            )
+        }
+
         setContent {
             OpenWeatherTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,22 +40,34 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    WeatherAppScreen(weatherDetailsViewModel)
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    OpenWeatherTheme {
-        Greeting("Android")
+    //TODO: Probably move this to a location permission wrapper
+    private val locationPermissionRequest = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        when {
+            permissions.getOrDefault(android.Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                weatherDetailsViewModel.getSavedLocationWeather()
+            }
+            permissions.getOrDefault(android.Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                weatherDetailsViewModel.getSavedLocationWeather()
+            }
+            else -> {
+            }
+        }
     }
+
+    //TODO: Duplicate here and in LocationService -> consolidate in location permission
+    private fun isLocationPermissionNotGranted(): Boolean = ContextCompat.checkSelfPermission(
+        this,
+        android.Manifest.permission.ACCESS_COARSE_LOCATION
+    ) != PackageManager.PERMISSION_GRANTED
 }
+
+
+
